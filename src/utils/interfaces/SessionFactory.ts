cd5d9343deb0d21bd8c0223d531171be503d8ed0
Lê Quốc Hải <lequochai26@gmail.com>
2024-01-17 01:40:51 +0700

    Modified SessionFactory interface.

--- src/utils/interfaces/SessionFactory.ts
+++ src/utils/interfaces/SessionFactory.ts
@@ -1,7 +1,8 @@
 import Session from "../Session";
 
 export default interface SessionFactory {
-    get(sessionId: string, createIfUndefined?: boolean): Session | undefined;
+    get(sessionId: string, createIfUndefined?: boolean): [ string, Session ] | undefined;
     create(sessionId: string): Session;
+    create(): [ string, Session ];
     clear(sessionId: string): void;
 }
