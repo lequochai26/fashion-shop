861fcd85ae4b44273e7684e3beb14d84b0c347ea
Lê Quốc Hải <lequochai26@gmail.com>
2024-01-17 01:49:18 +0700

    Simplify SessionFactory interface and add JSDoc for it.

--- src/utils/interfaces/SessionFactory.ts
+++ src/utils/interfaces/SessionFactory.ts
@@ -1,8 +1,24 @@
 import Session from "../Session";
 
+/**
+ * Bộ quản lý phiên làm việc của người dùng
+ */
 export default interface SessionFactory {
-    get(sessionId: string, createIfUndefined?: boolean): [ string, Session ] | undefined;
-    create(sessionId: string): Session;
+    /**
+     * Truy vấn phiên làm việc của người dùng
+     * @param sessionId Mã phiên làm việc (được lấy từ cookie máy khách)
+     */
+    get(sessionId: string): Session | undefined;
+
+    /**
+     * Tạo phiên làm việc mới
+     * @returns Mã phiên làm việc mới và phiên làm việc mới
+     */
     create(): [ string, Session ];
+
+    /**
+     * Xóa một phiên làm việc đã tồn tại
+     * @param sessionId Mã phiên làm việc (được lấy từ cookie máy khách)
+     */
     clear(sessionId: string): void;
 }
