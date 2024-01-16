4574dd410e30613c57d832051204776df02cdedf
Lê Quốc Hải <lequochai26@gmail.com>
2024-01-16 21:07:53 +0700

    Added SimpleSessionFactory implementation of SessionFactory.

---
+++ src/utils/SimpleSessionFactory.ts
@@ -0,0 +1,49 @@
+import Session from "./Session";
+import SessionFactory from "./interfaces/SessionFactory";
+
+export default class SimpleSessionFactory implements SessionFactory {
+    // Static methods:
+    public static generateId(): string {
+        return new Date()
+        .getTime()
+        .toString();
+    }
+
+    // Fields:
+    private sessionStorage: SessionStorage;
+
+    // Constructor:
+    public constructor() {
+        this.sessionStorage = {};
+    }
+
+    // Methods:
+    public get(sessionId: string, createIfUndefined?: boolean | undefined): Session | undefined {
+        if (!this.sessionStorage[sessionId]) {
+            if (createIfUndefined) {
+                sessionStorage[sessionId] = {};
+            }
+        }
+
+        return sessionStorage[sessionId];
+    }
+
+    public create(sessionId: string): Session {
+        if (sessionStorage[sessionId]) {
+            throw new Error(`Session with ID "${sessionId}" already exist!`);
+        }
+
+        sessionStorage[sessionId] = {};
+        return sessionStorage[sessionId];
+    }
+
+    public clear(sessionId: string): void {
+        if (!sessionStorage[sessionId]) {
+            throw new Error(`Session with ID "${sessionId} doesn't exist!"`);
+        }
+
+        sessionStorage[sessionId] = undefined;
+    }
+}
+
+type SessionStorage = { [ index: string ]: Session };
