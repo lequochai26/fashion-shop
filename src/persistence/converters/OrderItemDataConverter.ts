ccd07f0957c82519ecce3e94f8aec02f52546652
HoangKhuyen19 <104675608+HoangKhuyen19@users.noreply.github.com>
2024-02-20 15:24:48 +0700

    OrderItemDBHandler + OrderItemDataConverter

--- src/persistence/converters/OrderItemDataConverter.ts
+++ src/persistence/converters/OrderItemDataConverter.ts
@@ -2,7 +2,7 @@ import { Document, WithId } from "mongodb";
 import Converter from "../../utils/interfaces/Converter";
 import OrderItemData from "../data/OrderItemData";
 
-export default class OrderItemConverter implements Converter<WithId<Document>, OrderItemData>{
+export default class OrderItemDataConverter implements Converter<WithId<Document>, OrderItemData>{
     public convert(from: WithId<Document>): OrderItemData {
         return{
             orderId: from.orderId,
