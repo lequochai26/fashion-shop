90a54942115a97e96b492f6c5f377f6d88c43b71
ThanhHieu789 <neonkoro789@gmail.com>
2024-02-20 23:08:48 +0700

    Sửa 1 số trong ItemTypeData

--- src/persistence/ItemTypeDataDBHandler.ts
+++ src/persistence/ItemTypeDataDBHandler.ts
@@ -7,9 +7,11 @@ import { get, getAll, getByFilter, insert, remove, update } from "./Connector";
 export default class ItemTypeDataDBHandler implements DBHandler<ItemTypeData,string>{
     //static fileds các biến được khai báo với từ khóa static trong một lớp. Static fields chỉ có một bản sao duy nhất trong bộ nhớ 
     //và được chia sẻ bởi tất cả các đối tượng của lớp đó
-    private static itemTypeData: string = "ItemTypeData";
+    private static collectionNameItemType: string = "ItemTypeData";
+    
     //fields
     private itemTypeConverter: ItemTypeDataConverter;
+    
     //constructor
     public constructor(itemTypeConverter: ItemTypeDataConverter){
         this.itemTypeConverter = itemTypeConverter;
@@ -17,28 +19,36 @@ export default class ItemTypeDataDBHandler implements DBHandler<ItemTypeData,str
     //method
     public async get(pKey: string): Promise<ItemTypeData | undefined> {
         //tao doi tuong filter (ham get chi nhan 1 doi tuong)
-        const filter ={id:pKey}
+        const filter ={
+            id:pKey
+        }
+        
         //lấy document đầu tiên khớp với pKey (dùng filter) trong db.
         const documentItem: WithId<Document> | null = await get(
-            ItemTypeDataDBHandler.itemTypeData,filter
+            ItemTypeDataDBHandler.collectionNameItemType,filter
         );
+        
         //ko co thi return underfined
         if(!documentItem){
             return;
         }
+        
         //tim document phu hop voi pkey(dung filter)
         //chuyen document sang DTO
         const itemTypeData: ItemTypeData = this.itemTypeConverter.convert(documentItem);
+        
         //return, ben day chi viec goi ben Converter
         return itemTypeData;
     }
 
     public async getAll(): Promise<ItemTypeData[]> {
         const documentItems: WithId<Document>[] = await getAll(
-            ItemTypeDataDBHandler.itemTypeData
+            ItemTypeDataDBHandler.collectionNameItemType
         );
-         //tao ra 1 list chua dto,dcument la du lieu tho
+         
+        //tao ra 1 list chua dto,dcument la du lieu tho
         const itemTypeList: ItemTypeData[]=[];
+        
         //chuyen doi document sang dữ liệu DTO và thêm vào list
         for(const documentItem of documentItems){
             //cd
@@ -46,14 +56,18 @@ export default class ItemTypeDataDBHandler implements DBHandler<ItemTypeData,str
             //add
             itemTypeList.push(itemTypeData);
         }
+        
         return itemTypeList;
     }
+    
     public async getByFilter(filter: any): Promise<ItemTypeData[]> {
         const documentItems: WithId<Document>[] = await getByFilter(
-            ItemTypeDataDBHandler.itemTypeData,filter
+            ItemTypeDataDBHandler.collectionNameItemType,filter
         );
-         //tao ra 1 list chua dto,dcument la du lieu tho
+        
+        //tao ra 1 list chua dto,dcument la du lieu tho
         const itemTypeList: ItemTypeData[]=[];
+        
         //chuyen doi document sang dữ liệu DTO và thêm vào list
         for(const documentItem of documentItems){
             //cd
@@ -61,27 +75,33 @@ export default class ItemTypeDataDBHandler implements DBHandler<ItemTypeData,str
             //add
             itemTypeList.push(itemTypeData);
         }
+        
         return itemTypeList;
     }
+    
     public async insert(target: ItemTypeData): Promise<void> {
        //add, sau khi add thi thoat ra
-       return insert(ItemTypeDataDBHandler.itemTypeData,target);
+       return insert(ItemTypeDataDBHandler.collectionNameItemType,target);
     }
+    
     public async update(target: ItemTypeData): Promise<void> {
         //Tạo primary cho target(dùng filter để cập nhật document)
         const pKeyItem = { id: target.id };
-        return update(ItemTypeDataDBHandler.itemTypeData,target,pKeyItem);
+        return update(ItemTypeDataDBHandler.collectionNameItemType,target,pKeyItem);
     }
+    
     public async remove(target: ItemTypeData): Promise<void> {
 
         return this.removeByPrimaryKey(target.id);
     }
+    
     public async removeByPrimaryKey(pKey: string): Promise<void> {
         //xoa document khớp với primary key
         const primaryKey  = {
             id : pKey,
        };
-      return remove(ItemTypeDataDBHandler.itemTypeData,primaryKey);
+      
+       return remove(ItemTypeDataDBHandler.collectionNameItemType,primaryKey);
     }
     
 }
