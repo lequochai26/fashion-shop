5b78d607a92b97201c52a2f0c97e9f1735163210
ThanhHieu789 <neonkoro789@gmail.com>
2024-02-20 23:05:09 +0700

    Sửa 1 số đoạn trong BrandDBHandler

--- src/persistence/BrandDBHandler.ts
+++ src/persistence/BrandDBHandler.ts
@@ -7,38 +7,53 @@ import { get, getAll, getByFilter, insert, remove, update } from "./Connector";
 export default class BrandDataDBHandler implements DBHandler<BrandData,string>{
     //static fileds các biến được khai báo với từ khóa static trong một lớp. Static fields chỉ có một bản sao duy nhất trong bộ nhớ 
     //và được chia sẻ bởi tất cả các đối tượng của lớp đó
-    private static brandData: string = "BrandData";
+    private static collectioNameBrand: string = "BrandData";
+   
     //fields
     private brandDataConverter: BrandDataConverter;
+   
     //constructor
     public constructor(brandDataConverter: BrandDataConverter){
         this.brandDataConverter = brandDataConverter;
     }
+   
     public async get(pKey: string): Promise<BrandData | undefined> {
+        const filter = {id:pKey };
+       
         //lấy document đầu tiên khớp với pKey (dùng filter) trong db.
         const documentBrand: WithId<Document> | null = await get(
-            BrandDataDBHandler.brandData,pKey
+            BrandDataDBHandler.collectioNameBrand,
+            filter
         );
+        
         //ko co thi return underfined
         if(!documentBrand){
             return;
         }
+        
         //tim document phu hop voi pkey(dung filter)
         //chuyen document sang DTO
         const brandData: BrandData = this.brandDataConverter.convert(documentBrand);
+        
         //return, ben day chi viec goi ben Converter
         return brandData;
     }
+    
     public async getAll(): Promise<BrandData[]> {
+        
         const documentBrands: WithId<Document>[] = await getAll(
-            BrandDataDBHandler.brandData
-        );
-         //tao ra 1 list chua dto,dcument la du lieu tho
+            BrandDataDBHandler.collectioNameBrand      
+            );
+        
+        //tao ra 1 list chua dto,dcument la du lieu tho
         const brandList: BrandData[]=[];
+        
         //chuyen doi document sang dữ liệu DTO và thêm vào list
         for(const documentBrand of documentBrands){
             //cd
-            const brandData: BrandData = this.brandDataConverter.convert(documentBrand);
+            const brandData: BrandData = this //truy cap de truy van
+            .brandDataConverter.//truy van
+            convert(documentBrand); // gọi pt
             //add
             brandList.push(brandData);
         }
@@ -46,10 +61,12 @@ export default class BrandDataDBHandler implements DBHandler<BrandData,string>{
     }
     public async getByFilter(filter: any): Promise<BrandData[]> {
         const documentBrands: WithId<Document>[] = await getByFilter(
-            BrandDataDBHandler.brandData,filter
+            BrandDataDBHandler.collectioNameBrand,filter
         );
-         //tao ra 1 list chua dto,dcument la du lieu tho
+        
+        //tao ra 1 list chua dto(data transfer object),dcument la du lieu tho
         const brandList: BrandData[]=[];
+        
         //chuyen doi document sang dữ liệu DTO và thêm vào list
         for(const documentBrand of documentBrands){
             //cd
@@ -57,26 +74,33 @@ export default class BrandDataDBHandler implements DBHandler<BrandData,string>{
             //add
             brandList.push(brandData);
         }
+        
         return brandList;
     }
+
     public async insert(target: BrandData): Promise<void> {
         //add, sau khi add thi thoat ra
-       return insert(BrandDataDBHandler.brandData,target);
+       return insert(BrandDataDBHandler.collectioNameBrand,target);
     }
+
     public async update(target: BrandData): Promise<void> {
         //Tạo primary cho target(dùng filter để cập nhật document)
         const pKeyBrand = { id: target.id };
-        return update(BrandDataDBHandler.brandData,target,pKeyBrand);
+        
+        return update(BrandDataDBHandler.collectioNameBrand,target,pKeyBrand);
     }
+
     public async remove(target: BrandData): Promise<void> {
         return this.removeByPrimaryKey(target.id);
     }
+    
     public async removeByPrimaryKey(pKey: string): Promise<void> {
         //xoa document khớp với primary key
-        const primaryKey  = {
+        const primaryKey = {
             id : pKey,
-       };
-      return remove(BrandDataDBHandler.brandData,primaryKey);
+        };
+        
+        return remove(BrandDataDBHandler.collectioNameBrand,primaryKey);
     }
     
 }
