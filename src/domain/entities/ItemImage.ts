e6ce0505b9a94f58b198e81f21372c3118b54288
vongchutan3012 <vongchutan3012@gmail.com>
2024-02-21 08:26:10 +0700

    Thêm item image entity

---
+++ src/domain/entities/ItemImage.ts
@@ -0,0 +1,29 @@
+export default class ItemImage {
+
+    //Fields
+    //private item?
+    private path?: string | undefined;
+
+
+    //Constructor
+    public constructor(
+        path?: string | undefined
+    ) {
+        this.path = path;
+    }
+
+    //methods:
+    public get  Path(): string | undefined {
+        return this.path;
+    }
+
+    public set Path(path: string | undefined){
+        this.Path = path;
+    }
+    
+
+
+
+
+
+}
