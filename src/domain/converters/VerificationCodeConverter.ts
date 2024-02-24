25c3258401a8b21d6dccb6dc6d61f59c1ec06a2a
Lê Quốc Hải <lequochai26@gmail.com>
2024-02-24 22:10:23 +0700

    Implemented Converter for VerificationCode at Domain Layer.

---
+++ src/domain/converters/VerificationCodeConverter.ts
@@ -0,0 +1,34 @@
+import VerificationCodeData from "../../persistence/data/VerificationCodeData";
+import ReversableConverter from "../../utils/interfaces/ReversableConverter";
+import VerificationCode from "../entities/VerificationCode";
+
+export default class VerificationCodeConverter implements ReversableConverter<VerificationCodeData, VerificationCode> {
+    // Fields:
+
+    // Constructor:
+    public constructor() {
+
+    }
+
+    // Methods:
+    public convert(from: VerificationCodeData): VerificationCode {
+        const verificationCode = new VerificationCode();
+
+        verificationCode.Code = from.code;
+        verificationCode.Type = from.type;
+        verificationCode.CreationTime = from.creationTime;
+        verificationCode.LifeTime = from.lifeTime;
+
+        return verificationCode;
+    }
+
+    public reverse(from: VerificationCode): VerificationCodeData {
+        return {
+            email: from.User?.Email as string,
+            code: from.Code as string,
+            type: from.Type as string,
+            creationTime: from.CreationTime as string,
+            lifeTime: from.LifeTime as number
+        };
+    }
+}
