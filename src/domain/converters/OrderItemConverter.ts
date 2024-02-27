7a45670623c71c2ca8426c2be593ba7bf7129415
HoangKhuyen19 <104675608+HoangKhuyen19@users.noreply.github.com>
2024-02-27 10:31:36 +0700

    OrderItemConverter

---
+++ src/domain/converters/OrderItemConverter.ts
@@ -0,0 +1,42 @@
+import OrderItemData from "../../persistence/data/OrderItemData";
+import ReversableConverter from "../../utils/interfaces/ReversableConverter";
+import Item from "../entities/Item";
+import Order from "../entities/Order";
+import OrderItem from "../entities/OrderItem";
+
+export default class OrderItemConverter implements ReversableConverter<OrderItemData,OrderItem> {
+    //Fields:
+
+    //Constructor:
+    public constructor() {
+
+    }
+
+    //Methods:
+    convert(from: OrderItemData): OrderItem {
+        const orderItem : OrderItem = new OrderItem();
+
+        const order = new Order();
+        order.Id = from.orderId;
+        const item = new Item();
+        item.Id = from.itemId;
+
+        orderItem.Order = order;
+        orderItem.Item = item;
+        orderItem.Amount = from.amount;
+        orderItem.Price = from.price;
+        orderItem.Metadata = from.metadata;
+
+        return orderItem;
+    }
+
+    reverse(from: OrderItem): OrderItemData {
+        return{
+            orderId: from.Order?.Id as string,
+            itemId: from.Item?.Id as string,
+            amount: from.Amount as number,
+            price: from.Price as number,
+            metadata: from.Metadata as string
+        };
+    }
+}
