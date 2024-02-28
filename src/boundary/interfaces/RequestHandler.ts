6cf5c2e5cb8bdf91bf4eb7a854b1eeb58716b02f
Lê Quốc Hải <lequochai26@gmail.com>
2024-02-29 01:19:33 +0700

    Added getPath method for RequestHandler interface of Boundary layer.

--- src/boundary/interfaces/RequestHandler.ts
+++ src/boundary/interfaces/RequestHandler.ts
@@ -5,4 +5,5 @@ export default interface RequestHandler {
     post(request: Request, response: Response): void;
     put(request: Request, response: Response): void;
     del(request: Request, response: Response): void;
+    getPath(): string;
 }
