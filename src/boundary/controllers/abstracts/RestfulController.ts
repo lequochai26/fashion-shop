2dc2ee169946089101cb482deb51333b75cf7467
Lê Quốc Hải <lequochai26@gmail.com>
2024-02-29 03:53:26 +0700

    Added useDomainManager protected method for RestfulController abstract class.

--- src/boundary/controllers/abstracts/RestfulController.ts
+++ src/boundary/controllers/abstracts/RestfulController.ts
@@ -11,6 +11,17 @@ export default abstract class RestfulController implements Controller<RestfulCon
         this.domainManager = domainManager;
     }
 
+    // Protected methods:
+    protected async useDomainManager<T>(
+        executable: (domainManager: DomainManager) => Promise<T>
+    ): Promise<T> {
+        if (!this.domainManager) {
+            throw new Error("domainManager field is missing!");
+        }
+
+        return executable(this.domainManager);
+    }
+
     // Methods:
     public abstract execute(param: RestfulControllerParam): Promise<void>;
 }
