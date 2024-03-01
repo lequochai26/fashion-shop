e83f5166f3a5b3ad154b0412284638aea3eeb929
Lê Quốc Hải <lequochai26@gmail.com>
2024-03-02 03:03:00 +0700

    Added ItemInfo for boundary layer.

---
+++ src/boundary/infos/item/ItemInfo.ts
@@ -0,0 +1,15 @@
+import ItemInfoBrand from "./ItemInfoBrand";
+import ItemInfoItemType from "./ItemInfoItemType";
+
+export default interface ItemInfo {
+    id: string;
+    avatar: string;
+    name: string;
+    description: string;
+    price: number;
+    amount: number;
+    gender: boolean;
+    metadata?: string | undefined;
+    type?: ItemInfoItemType | undefined;
+    brand?: ItemInfoBrand | undefined;
+}
