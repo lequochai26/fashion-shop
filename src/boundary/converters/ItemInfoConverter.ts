fa1059f85ef7fb562450aaa670e2eeaecf916ead
Lê Quốc Hải <lequochai26@gmail.com>
2024-03-05 02:44:34 +0700

    Modified ItemInfoConverter.

--- src/boundary/converters/ItemInfoConverter.ts
+++ src/boundary/converters/ItemInfoConverter.ts
@@ -18,7 +18,7 @@ export default class ItemInfoConverter implements Converter<Item, ItemInfo> {
             price: from.Price as number,
             amount: from.Amount as number,
             gender: from.Gender as boolean,
-            metadata: (from.MetaData ? JSON.parse(from.MetaData) : undefined),
+            metadata: from.MetaData,
             type: (
                 from.Type
                 ? {
