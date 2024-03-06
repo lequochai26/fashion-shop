27a651de91cd4a9420b24a182661928711041502
Lê Quốc Hải <lequochai26@gmail.com>
2024-03-07 01:25:12 +0700

    Added deleteFileController field for UpdateItemRestfulController.

--- src/boundary/controllers/abstracts/UpdateItemRestfulController.ts
+++ src/boundary/controllers/abstracts/UpdateItemRestfulController.ts
@@ -1,4 +1,5 @@
 import DomainManager from "../../../domain/DomainManager";
+import DeleteFileController from "../DeleteFileController";
 import WriteFileNamingByDateTimeController from "../WriteFileNamingByDateTimeController";
 import Controller from "../interfaces/Controller";
 import RestfulController from "./RestfulController";
@@ -6,11 +7,14 @@ import RestfulController from "./RestfulController";
 export default abstract class UpdateItemRestfulController extends RestfulController {
     // Fields:
     protected writeFileNamingByDateTimeController: Controller<{ destination: string, extension: string, buffer: Buffer }, string>;
+    protected deleteFileController: Controller<string, void>;
 
     // Constructor:
     public constructor(domainManager?: DomainManager | undefined) {
         super(domainManager);
 
         this.writeFileNamingByDateTimeController = new WriteFileNamingByDateTimeController();
+
+        this.deleteFileController = new DeleteFileController();
     }
 }
