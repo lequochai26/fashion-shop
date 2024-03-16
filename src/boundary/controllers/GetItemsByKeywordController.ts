2b512e553fa4fd128a74903b6254b4052af43bc4
Lê Quốc Hải <lequochai26@gmail.com>
2024-03-16 17:25:16 +0700

    Modified GetItemsByKeywordController to receive keyword through query string.

--- src/boundary/controllers/GetItemsByKeywordController.ts
+++ src/boundary/controllers/GetItemsByKeywordController.ts
@@ -18,7 +18,7 @@ export default class GetItemsByKeywordController extends QueryItemRestfulControl
     //Methods
     public async execute({ request, response}: RestfulControllerParam): Promise<void> {
         //Get keyword 
-        const keyword: string | undefined = request.body.keyword as string;
+        const keyword: string | undefined = request.query.keyword as string | undefined;
 
         if(!keyword) {
             response.json (
