89aa4cbe58a527b45428984615df79062669cfad
quyhaodoan <140572237+quyhaodoan@users.noreply.github.com>
2024-03-23 13:17:24 +0700

    fix brandInfoConverter

--- src/boundary/controllers/GetAllBrandsController.ts
+++ src/boundary/controllers/GetAllBrandsController.ts
@@ -1,16 +1,16 @@
 import DomainManager from "../../domain/DomainManager";
 import Brand from "../../domain/entities/Brand";
+import Converter from "../../utils/interfaces/Converter";
 import ReversableConverter from "../../utils/interfaces/ReversableConverter";
 import BrandInfo from "../infos/brand/BrandInfo";
 import BrandQueryRestfulController from "./abstracts/BrandQueryRestfulController";
 import RestfulControllerParam from "./interfaces/RestfulControllerParam";
-import { response } from "express";
 
 
 export default class GetAllBrandsController extends BrandQueryRestfulController{
     //Constructor
     public constructor(
-        brandInfoConverter: ReversableConverter<Brand, BrandInfo>,
+        brandInfoConverter: Converter<Brand, BrandInfo>,
         domainManager?: DomainManager | undefined
     ){
         super(brandInfoConverter,domainManager);
