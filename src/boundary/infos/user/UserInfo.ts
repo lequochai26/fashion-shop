00b6ed397b903861495bfe1aa9de3199ab78c3ad
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-08 00:17:49 +0700

    Added UserInfo.

---
+++ src/boundary/infos/user/UserInfo.ts
@@ -0,0 +1,11 @@
+export default interface UserInfo {
+    email: string;
+    fullName: string;
+    gender: boolean;
+    phoneNumber: string;
+    address: string;
+    avatar: string;
+    permission: string;
+    orderedOrders: string[];
+    createdOrders: string[];
+}
