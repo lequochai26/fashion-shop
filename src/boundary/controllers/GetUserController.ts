359b6e3a04df3d00ff49acad998034e105fe9428
vongchutan3012 <vongchutan3012@gmail.com>
2024-04-08 13:23:51 +0700

    Fixed in class GetUserController

--- src/boundary/controllers/GetUserController.ts
+++ src/boundary/controllers/GetUserController.ts
@@ -29,48 +29,37 @@ export default class GetUserController extends QueryUserRestfulController {
                     code: "EMAIL_REQUIRED"
                 }
             );
+            return;
+        }
+        //path
+        const path: any = [] = [];
 
-            //path
-            const path: any = [] = [];
-
-            try {
-                var user: User | undefined =
-                    await this.useDomainManager( //This = Su dung useDomainManager trong GetItem
-                        async function (useDomainManager) {
-                            return useDomainManager.getUser(email, path)
-                        }
-                    )
-            } catch (error: any) {
-                console.log(error);
-
-                //Trong luong catch
-                response.json(
-                    {
-                        success: false,
-                        message: "Failed while handling with DB!"
+        try {
+            var user: User | undefined =
+                await this.useDomainManager( //This = Su dung useDomainManager trong GetItem
+                    async function (useDomainManager) {
+                        return useDomainManager.getUser(email, path)
                     }
                 )
-                return;
-            }
+        } catch (error: any) {
+            console.log(error);
 
-
-            if (!user) {
-                response.json(
-                    {
-                        success: false,
-                        message: "not found this user",
-                        code: "user required"
-                    }
-                )
-                return;
-            }
-
+            //Trong luong catch
             response.json(
                 {
-                    success: true,
-                    result: user && this.userInfoConverter.convert(user)
+                    success: false,
+                    message: "Failed while handling with DB!",
+                    code: "HANDLING_DB_FAILED"
                 }
             )
+            return;
         }
+
+        response.json(
+            {
+                success: true,
+                result: user && this.userInfoConverter.convert(user)
+            }
+        );
     }
 }
