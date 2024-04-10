a2d56b3ba79869a7a32c70f316d150557e79a972
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-10 23:12:16 +0700

    Added DomainManagerHolder base class.

---
+++ src/boundary/controllers/base_classes/DomainManagerHolder.ts
@@ -0,0 +1,33 @@
+import DomainManager from "../../../domain/DomainManager";
+
+export default class DomainManagerHolder {
+    // Fields:
+    protected domainManager?: DomainManager | undefined;
+
+    // Constructors:
+    public constructor(
+        domainManager?: DomainManager | undefined
+    ) {
+        this.domainManager = domainManager;
+    }
+
+    // Protected methods:
+    protected async useDomainManager<T>(
+        executable: (domainManager: DomainManager) => Promise<T>
+    ): Promise<T> {
+        if (!this.domainManager) {
+            throw new Error("domainManager field is missing!");
+        }
+
+        return executable(this.domainManager);
+    }
+
+    // Getters / setters:
+    public get DomainManager(): DomainManager | undefined {
+        return this.domainManager;
+    }
+
+    public set DomainManager(domainManager: DomainManager | undefined) {
+        this.domainManager = domainManager;
+    }
+}
