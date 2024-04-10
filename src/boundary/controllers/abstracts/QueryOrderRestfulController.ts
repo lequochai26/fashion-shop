8825aedaefa7ecf077216efad32a6ceff708515d
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-11 01:02:20 +0700

    Modified QueryOrderRestfulController to extend to PermissionRequiredController.

--- src/boundary/controllers/abstracts/QueryOrderRestfulController.ts
+++ src/boundary/controllers/abstracts/QueryOrderRestfulController.ts
@@ -2,9 +2,9 @@ import DomainManager from "../../../domain/DomainManager";
 import Order from "../../../domain/entities/Order";
 import Converter from "../../../utils/interfaces/Converter";
 import OrderInfo from "../../infos/order/OrderInfo";
-import RestfulController from "./RestfulController";
+import PermissionRequiredController from "../PermissionRequiredController";
 
-export default abstract class QueryOrderRestfulController extends RestfulController {
+export default abstract class QueryOrderRestfulController extends PermissionRequiredController {
     // Fields 
     protected orderInfoConverter: Converter<Order, OrderInfo>;
 
