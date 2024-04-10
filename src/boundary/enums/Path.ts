923f5c280c377c8d9c3f670b5f57b09eae8ba9bd
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-10 14:08:59 +0700

    Modified Path enum.

--- src/boundary/enums/Path.ts
+++ src/boundary/enums/Path.ts
@@ -1,6 +1,7 @@
 enum Path {
-    AVATAR_LOCAL_PATH = "./assets/avatar",
-    AVATAR_HTTP_PATH = "/assets/avatar"
+    USER_AVATAR_LOCAL_PATH = "./assets/avatar",
+    USER_AVATAR_HTTP_PATH = "/assets/avatar",
+    DEFAULT_USER_AVATAR_HTTP_PATH = "/assets/avatar/default.png"
 }
 
 export default Path;
