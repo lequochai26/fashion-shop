b76086b448d7a510681047a334d50c62f453e690
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-12 03:17:16 +0700

    Added QueryCartRestfulController abstract class.

---
+++ src/boundary/controllers/abstracts/QueryCartRestfulController.ts
@@ -0,0 +1,20 @@
+import DomainManager from "../../../domain/DomainManager";
+import CartItem from "../../../domain/entities/CartItem";
+import Converter from "../../../utils/interfaces/Converter";
+import CartItemInfo from "../../infos/cartitem/CartItemInfo";
+import PermissionRequiredRestfulController from "../PermissionRequiredRestfulController";
+
+export default abstract class QueryCartRestfulController extends PermissionRequiredRestfulController {
+    // Fields:
+    protected cartItemInfoConverter: Converter<CartItem, CartItemInfo>;
+
+    // Constructors:
+    public constructor(
+        cartItemInfoConverter: Converter<CartItem, CartItemInfo>,
+        domainManager?: DomainManager | undefined
+    ) {
+        super(domainManager);
+
+        this.cartItemInfoConverter = cartItemInfoConverter;
+    }
+}
