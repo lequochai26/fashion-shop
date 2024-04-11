596b2c15d209a66d2cd2f0978b2ccecb0872ee38
Lê Quốc Hải <lequochai26@gmail.com>
2024-04-11 16:15:25 +0700

    Make NewOrderRestfulController composition to NewOrderValidateController.

--- src/boundary/controllers/abstracts/NewOrderRestfulController.ts
+++ src/boundary/controllers/abstracts/NewOrderRestfulController.ts
@@ -3,17 +3,6 @@ import NewOrderValidateController from "../NewOrderValidateController";
 import RestfulController from "./RestfulController";
 
 export default abstract class NewOrderRestfulController extends RestfulController {
-    // Static fields:
-    private static newOrderValidateController: NewOrderValidateController;
-
-    private static getNewOrderValidateController(domainManager?: DomainManager | undefined) {
-        if (!NewOrderRestfulController.newOrderValidateController) {
-            NewOrderRestfulController.newOrderValidateController = new NewOrderValidateController(domainManager);
-        }
-
-        return NewOrderRestfulController.newOrderValidateController;
-    }
-
     // Fields:
     protected validateController: NewOrderValidateController;
 
@@ -22,6 +11,6 @@ export default abstract class NewOrderRestfulController extends RestfulControlle
         domainManager?: DomainManager | undefined
     ) {
         super(domainManager);
-        this.validateController = NewOrderRestfulController.getNewOrderValidateController(domainManager);
+        this.validateController = new NewOrderValidateController(domainManager);
     }
 }
