5f8d775d29ba5576eb73db7697847f4bef6d5c05
ThanhHieu789 <neonkoro789@gmail.com>
2024-04-24 00:48:23 +0700

    trigger delete itemtype

---
+++ scripts/hqtcsdl/triggers/ItemType/triggerInsteadOfDeleteItemType.ts
@@ -0,0 +1,13 @@
+import Item from "../../collections/Item";
+import Trigger from "../../types/Trigger";
+
+const triggerInsteadOfDeleteItemType: Trigger = async function (inserted,deleted) {
+    //select item
+    const items: any[] = await Item.select({itemType: deleted.id});
+
+    if(items.length >=30){
+        throw new Error(`Loại sản phẩm này đang có hơn 30 sản phẩm được bán,không thể xóa !`)
+    }
+}
+
+export default triggerInsteadOfDeleteItemType;
