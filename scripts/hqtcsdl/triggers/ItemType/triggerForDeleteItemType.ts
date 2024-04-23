800e6fbe52e3641756067693d722996651514a2b
ThanhHieu789 <neonkoro789@gmail.com>
2024-04-24 01:07:20 +0700

    trigger for deleteItemType

---
+++ scripts/hqtcsdl/triggers/ItemType/triggerForDeleteItemType.ts
@@ -0,0 +1,25 @@
+import Item from "../../collections/Item";
+import Trigger from "../../types/Trigger";
+
+const triggerForDeleteItemType: Trigger = async function (inserted,deleted) {
+    //Cập nhật itemtye field của Items thuộc ItemType này thành undefined
+    const items: any[] = await Item.select({type : inserted.id});
+
+    for (const item of items){
+        await Item.update(
+            item.id,
+            item.avatar,
+            item.name,
+            item.description,
+            item.price,
+            item.buyPrice,
+            item.amount,
+            item.gender,
+            item.metadata,
+            item.brand,
+            undefined
+        );
+    }
+}
+
+export default triggerForDeleteItemType;
