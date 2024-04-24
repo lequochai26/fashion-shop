faa82c8d2d9138dbef4c7a56a8eaaa5ab2a3b4d6
ThanhHieu789 <neonkoro789@gmail.com>
2024-04-25 00:36:50 +0700

    fix procedureDeleteitem

--- scripts/hqtcsdl/stored_procedures/Item/procedureDeleteItem.ts
+++ scripts/hqtcsdl/stored_procedures/Item/procedureDeleteItem.ts
@@ -4,10 +4,10 @@ import Item from "../../collections/Item";
 export default async function procedureDeleteItem(id:string):Promise<void>{
    
   try {
-    if(!await Item.select({id})){
+    if((await Item.select({id})).length === 0){
       throw new Error(`Không tồn tại sản phẩm với mã ${id}`)
     }
-    return Item.delete(id);
+    await Item.delete(id);
   } catch (error:any) {
     console.error("Lỗi khi xóa ",error);
   }
