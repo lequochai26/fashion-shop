5d9aa540bd2b927b6d97acc9b985602aefaa5432
HoangKhuyen19 <104675608+HoangKhuyen19@users.noreply.github.com>
2024-04-24 22:37:01 +0700

    modified viewItemsPriceBetween250_500

--- scripts/hqtcsdl/views/Item/viewItemsPriceBetween250_500.ts
+++ scripts/hqtcsdl/views/Item/viewItemsPriceBetween250_500.ts
@@ -4,7 +4,7 @@ export default async function viewItemsPriceBetween250_500(): Promise<any[]> {
     //Select items
     let items: any[] = await Item.select();
 
-    items = items.filter((item)=> (item.price > 250 && item.price <500));
+    items = items.filter((item)=> (item.price >= 250 && item.price <= 500));
 
     return items;
 }
