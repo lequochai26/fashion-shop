58d8ac9c96e054e578e2b4816745d950309b2cd1
quyhaodoan <140572237+quyhaodoan@users.noreply.github.com>
2024-04-25 01:44:17 +0700

    fixed

--- scripts/hqtcsdl/stored_procedures/ItemType/procedureGetItemTypeWithNameStartByS.ts
+++ scripts/hqtcsdl/stored_procedures/ItemType/procedureGetItemTypeWithNameStartByS.ts
@@ -1,5 +1,5 @@
 import ItemType from "../../collections/ItemType";
-import User from "../../collections/User";
+
 
 export default async function procedureGetItemTypeWithNameStartByS():Promise<any> {
     return ItemType.select(
