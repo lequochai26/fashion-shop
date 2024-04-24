a6af7888b80fcc3a42726716aa0f41e13940e1ec
vongchutan3012 <vongchutan3012@gmail.com>
2024-04-24 10:51:55 +0700

    added procedureUpdateItem

--- scripts/hqtcsdl/stored_procedures/Item/procedureUpdateItem.ts
+++ scripts/hqtcsdl/stored_procedures/Item/procedureUpdateItem.ts
@@ -1,5 +1,9 @@
 import Item from "../../collections/Item";
 
-export default async function procedureUpdateItem(id:string){
-
+export default async function procedureUpdateItem(id: string) {
+    try {
+        await Item.update(id);
+    } catch (error: any) {
+        console.error("Đã xảy ra cập nhật sản phẩm", error);
+    }
 }
