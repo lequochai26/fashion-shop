a7e9a506790278db756f9bfeb89922df0db2a06d
vongchutan3012 <vongchutan3012@gmail.com>
2024-04-24 23:45:47 +0700

    Sửa triggerInsteadOfInsert lại, thêm điều kiện type và brand phù hợp

--- scripts/hqtcsdl/triggers/Item/triggerInsteadOfInsertItem.ts
+++ scripts/hqtcsdl/triggers/Item/triggerInsteadOfInsertItem.ts
@@ -1,4 +1,6 @@
+import Brand from "../../collections/Brand";
 import Item from "../../collections/Item";
+import ItemType from "../../collections/ItemType";
 import Trigger from "../../types/Trigger";
 
 const triggerInsteadOfInsertItem: Trigger = async function (inserted, deleted): Promise<void> {
@@ -13,7 +15,7 @@ const triggerInsteadOfInsertItem: Trigger = async function (inserted, deleted): 
     const amount: number = inserted.amount;
     const gender: boolean = inserted.gender;
     const type: string | undefined = inserted.type;
-    const brand: string | undefined = inserted.type;
+    const brand: string | undefined = inserted.brand;
 
 
     //Kiểm tra id
@@ -25,48 +27,44 @@ const triggerInsteadOfInsertItem: Trigger = async function (inserted, deleted): 
         throw new Error(`Đã tồn tại sản phẩm với mã ${id} !`);
     }
 
-    //Kiểm tra avatar
-    if (!avatar) {
-        throw new Error("")
-    }
-
     //Kiểm tra tên
     if (!name) {
         throw new Error(`Tên sản phẩm không được rỗng!`);
     }
 
     //kiểm tra giá
-    if (price <= 0) {
+    if (price < 0) {
         throw new Error("Giá bán phải lớn hơn không")
     }
 
     //priceBuy check
-    if (buyPrice <= 0) {
+    if (buyPrice < 0) {
         throw new Error("Giá mua lại phải lớn hơn không")
     }
     //kiểm tra số lượng
-    if (amount <= 0) {
-        throw new Error(" số lượng phải lớn hơn không")
+    if (amount < 0) {
+        throw new Error("Số lượng phải lớn hơn không")
     }
 
     //kiểm tra mô tả
     if (!description) {
-        throw new Error(" không được để trống mô tả")
+        throw new Error("Không được để trống mô tả")
     }
 
-
-    //kiểm tra gender
-    if (!gender) {
-        throw new Error(" phải chọn giới tính ")
-    }
-
-    if (!type) {
-        throw new Error(" Phải có loại sản phẩm phù hợp ")
+    if (type) {
+        const [itemType] = await ItemType.select({ id: type });
+        if (!itemType) {
+            throw new Error(`Loại sản phẩm với mã "${type}" không tồn tại`)
+        }
 
     }
 
-    if(!brand){
-        throw new Error ("không được để trống thương hiệu")
+    if (brand) {
+        const [idBrand] = await Brand.select({ id: brand });
+        if (!idBrand) {
+            throw new Error(`Thương hiệu với mã "${brand}" không tồn tại`)
+        }
+
     }
 }
 
