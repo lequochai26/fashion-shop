54ee2889c7c552c243d93719becd487ee84921d5
vongchutan3012 <vongchutan3012@gmail.com>
2024-04-25 00:55:26 +0700

    Sửa triggerInsteadOfUpdateItem

--- scripts/hqtcsdl/triggers/Item/triggerInsteadOfUpdateItem.ts
+++ scripts/hqtcsdl/triggers/Item/triggerInsteadOfUpdateItem.ts
@@ -1,7 +1,10 @@
+import Brand from "../../collections/Brand";
+import Item from "../../collections/Item";
+import ItemType from "../../collections/ItemType";
 import Trigger from "../../types/Trigger";
 const triggerInsteadOfUpdateItem: Trigger = async function (inserted, deleted): Promise<void> {
 
-    
+
     const name: string = inserted.name;
     const description: string = inserted.description;
     const price: number = inserted.price;
@@ -12,21 +15,7 @@ const triggerInsteadOfUpdateItem: Trigger = async function (inserted, deleted): 
 
     //Kiểm tra tên
     if (!name) {
-        throw new Error(`Tên sản phẩm không được rỗng!`);
-    }
-
-    //kiểm tra giá
-    if (price <= 0) {
-        throw new Error("Giá bán phải lớn hơn không")
-    }
-
-    //priceBuy check
-    if (buyPrice <= 0) {
-        throw new Error("Giá mua lại phải lớn hơn không")
-    }
-    //kiểm tra số lượng
-    if (amount <= 0) {
-        throw new Error(" số lượng phải lớn hơn không")
+        throw new Error("Tên sản phẩm không được rỗng")
     }
 
     //kiểm tra mô tả
@@ -34,11 +23,38 @@ const triggerInsteadOfUpdateItem: Trigger = async function (inserted, deleted): 
         throw new Error(" không được để trống mô tả")
     }
 
+    //kiểm tra giá
+    if (price < 0) {
+        throw new Error("Giá bán phải lớn hơn không")
+    }
 
-    //Tồn tại mã tương ứng với type
+    //priceBuy check
+    if (buyPrice < 0) {
+        throw new Error("Giá mua lại phải lớn hơn không")
+    }
+    //kiểm tra số lượng
+    if (amount < 0) {
+        throw new Error(" số lượng phải lớn hơn không")
+    }
 
-    //Tồn tại mã tương ứng với brand
-    
+
+
+    if (type) {
+        const [itemType] = await ItemType.select({ id: type });
+        if (!itemType) {
+            throw new Error(`Loại sản phẩm với mã "${type}" không tồn tại`)
+        }
+
+    }
+
+    if (brand) {
+        const [idBrand] = await Brand.select({ id: brand });
+        if (!idBrand) {
+            throw new Error(`Thương hiệu với mã "${brand}" không tồn tại`)
+        }
+
+    }
+
 
 }
 
