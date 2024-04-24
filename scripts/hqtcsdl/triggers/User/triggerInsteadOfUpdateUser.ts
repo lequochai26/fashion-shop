459c85eef326ee557237369edbc34c2ce6adff81
HoangKhuyen19 <104675608+HoangKhuyen19@users.noreply.github.com>
2024-04-24 23:09:48 +0700

    modified trigger instead of update user

--- scripts/hqtcsdl/triggers/User/triggerInsteadOfUpdateUser.ts
+++ scripts/hqtcsdl/triggers/User/triggerInsteadOfUpdateUser.ts
@@ -9,46 +9,59 @@ const triggerInsteadOfUpdateUser: Trigger = async function (inserted, deleted): 
     }
 
     //Fullname
-    const fullName: string = inserted.fullName;
-    if (!fullName) {
-        throw new Error("Họ tên không được để trống!")
-    }
+    const fullName: string | undefined = inserted.fullName;
+    if (fullName !== undefined) {
+        if (!fullName) {
+            throw new Error("Họ tên không được để trống!")
+        }
 
-    if (fullName.length <= 5) {
-        throw new Error("Họ tên phải có ít nhất 5 ký tự!");
+        if (fullName.length <= 5) {
+            throw new Error("Họ tên phải có ít nhất 5 ký tự!");
+        }
     }
 
     //Phonenumber check
-    const phoneNumer = inserted.phoneNumber;
-    if (!phoneNumer) {
-        throw new Error("Số điện thoại không được để trống!");
+    const phoneNumer: string | undefined = inserted.phoneNumber;
+    if (phoneNumer !== undefined) {
+        if (!phoneNumer) {
+            throw new Error("Số điện thoại không được để trống!");
+        }
+
+        const regexPhoneNumber: RegExp = /^(0|\+84)(\d{9,10})$/;
+        if (!regexPhoneNumber.test(phoneNumer)) {
+            throw new Error("Số điện thoại không hợp lệ!");
+        }
     }
 
-    const regexPhoneNumber: RegExp = /^(0|\+84)(\d{9,10})$/;
-    if (!regexPhoneNumber.test(phoneNumer)) {
-        throw new Error("Số điện thoại không hợp lệ!");
-    }
+
 
     //Address check
-    const address: string =  inserted.address;
-    if(!address) {
-        throw new Error("Địa chỉ không được để trống!")
+    const address: string | undefined = inserted.address;
+    if(address !== undefined) {
+        if (!address) {
+            throw new Error("Địa chỉ không được để trống!")
+        }
+    
+        if (address.length <= 25) {
+            throw new Error("Địa chỉ phải có ít nhất 25 ký tự!");
+        }
     }
 
-    if(fullName.length <= 25) {
-        throw new Error("Địa chỉ phải có ít nhất 25 ký tự!");
-    }
 
     //Avatar
-    const avatar = inserted.avatar;
-    if(!avatar) {
-        throw new Error("Ảnh đại diện không được để trống!");
+    const avatar: string | undefined = inserted.avatar;
+    if(avatar !== undefined) {
+        if (!avatar) {
+            throw new Error("Ảnh đại diện không được để trống!");
+        }
     }
 
     //Permissio check
     const permission: string = inserted.permission;
-    if(!["CUSTOMER", "EMPLOYEE", "MANAGER"].includes(permission)){
-        throw new Error("Quyền truy cập không hợp lệ!");
+    if(permission !== undefined) {
+        if (!["CUSTOMER", "EMPLOYEE", "MANAGER"].includes(permission)) {
+            throw new Error("Quyền truy cập không hợp lệ!");
+        }
     }
 }
 
