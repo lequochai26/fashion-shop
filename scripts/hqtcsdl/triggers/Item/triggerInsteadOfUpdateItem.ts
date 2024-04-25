d70addbd72fb0b9010e43a8d69c787cf1fbad06f
vongchutan3012 <vongchutan3012@gmail.com>
2024-04-25 10:48:04 +0700

    triggerInsteadOfUpdateItem còn dang dở

--- scripts/hqtcsdl/triggers/Item/triggerInsteadOfUpdateItem.ts
+++ scripts/hqtcsdl/triggers/Item/triggerInsteadOfUpdateItem.ts
@@ -4,41 +4,44 @@ import ItemType from "../../collections/ItemType";
 import Trigger from "../../types/Trigger";
 const triggerInsteadOfUpdateItem: Trigger = async function (inserted, deleted): Promise<void> {
 
-
-    const name: string = inserted.name;
-    const description: string = inserted.description;
-    const price: number = inserted.price;
-    const buyPrice: number = inserted.buyPrice;
-    const amount: number = inserted.amount;
-    const type: string | undefined = inserted.type;
-    const brand: string | undefined = inserted.type;
-
     //Kiểm tra tên
-    if (!name) {
-        throw new Error("Tên sản phẩm không được rỗng")
+    const name: string = inserted.name;
+    if (name) {
+        if (!name) {
+            throw new Error("Tên sản phẩm không được rỗng")
+        }
     }
+
 
     //kiểm tra mô tả
-    if (!description) {
-        throw new Error(" không được để trống mô tả")
+    const description: string = inserted.description;
+    if (description) {
+        if (!description) {
+            throw new Error(" không được để trống mô tả")
+        }
     }
 
+
     //kiểm tra giá
+    const price: number = inserted.price;
     if (price < 0) {
         throw new Error("Giá bán phải lớn hơn không")
     }
 
     //priceBuy check
+    const buyPrice: number = inserted.buyPrice;
     if (buyPrice < 0) {
         throw new Error("Giá mua lại phải lớn hơn không")
     }
+
     //kiểm tra số lượng
+    const amount: number = inserted.amount;
     if (amount < 0) {
         throw new Error(" số lượng phải lớn hơn không")
     }
 
-
-
+    //Kiểm tra type
+    const type: string | undefined = inserted.type;
     if (type) {
         const [itemType] = await ItemType.select({ id: type });
         if (!itemType) {
@@ -47,15 +50,13 @@ const triggerInsteadOfUpdateItem: Trigger = async function (inserted, deleted): 
 
     }
 
+    //Kiểm tra brand
+    const brand: string | undefined = inserted.type;
     if (brand) {
         const [idBrand] = await Brand.select({ id: brand });
         if (!idBrand) {
             throw new Error(`Thương hiệu với mã "${brand}" không tồn tại`)
         }
-
     }
-
-
 }
-
 export default triggerInsteadOfUpdateItem;
